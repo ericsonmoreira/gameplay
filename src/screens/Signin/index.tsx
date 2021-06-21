c4a08395169a577af380009c225820d668251375
ericsonmoreira <moreira.ericson@gmail.com>
2021-06-20 21:09:59 -0300

    add signin screen

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from './styles';
+
+const Signin: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Ola Mundo!</Text>
+    </View>
+  );
+};
+
+export default Signin;
