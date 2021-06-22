af913d07ae8990db2258dfae1ddb12a1a3ed76d1
ericsonmoreira <moreira.ericson@gmail.com>
2021-06-21 22:13:25 -0300

    add google fonts expo

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
+import { Inter_400Regular, Inter_500Medium, Inter_700Bold } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 import React from 'react';
-import Signin from './src/screens/Signin';
 import { ThemeProvider } from 'styled-components';
+import Signin from './src/screens/Signin';
 import theme from './src/theme';
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Signin />
-    </ThemeProvider>
-  );
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, Inter_500Medium, Inter_700Bold, Rajdhani_500Medium, Rajdhani_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <ThemeProvider theme={theme}>
+        <Signin />
+      </ThemeProvider>
+    );
+  } 
 }
